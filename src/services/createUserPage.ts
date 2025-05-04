74ec5dd46e7fe3ab5fa35b589cc1fb52f7397365
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2025-05-04 18:03:18 +0200

    Update createUserPage.ts

--- src/services/createUserPage.ts
+++ src/services/createUserPage.ts
@@ -46,8 +46,9 @@ export default async (u: SlideskLinkUser) => {
             <blockquote>${md.render(p.abstract ?? "")}</blockquote>
         </details>
         <footer>
-            ${p.Session.map(
-              (s, _) => `
+            ${p.Session.sort((a, b) => Number(a.date) - Number(b.date))
+              .map(
+                (s, _) => `
                 <div>
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar-days-icon lucide-calendar-days"><path d="M8 2v4"/><path d="M16 2v4"/><rect width="18" height="18" x="3" y="4" rx="2"/><path d="M3 10h18"/><path d="M8 14h.01"/><path d="M12 14h.01"/><path d="M16 14h.01"/><path d="M8 18h.01"/><path d="M12 18h.01"/><path d="M16 18h.01"/></svg>
                   ${new Date(s.date).toDateString()}: ${s.location}
@@ -111,7 +112,8 @@ export default async (u: SlideskLinkUser) => {
                   }
                 </div>
               `,
-            ).join("")}
+              )
+              .join("")}
         </footer>
     </article>
     `);
