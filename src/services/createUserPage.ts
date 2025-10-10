c0dc3cfa32ef3f10ca46fa73e87aaf0de1f98132
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2025-10-10 15:55:16 +0200

    Add footer links to user page template

--- src/services/createUserPage.ts
+++ src/services/createUserPage.ts
@@ -240,7 +240,41 @@ export default async (u: SlideskLinkUser) => {
   //<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-palette-icon lucide-palette"><path d="M12 22a1 1 0 0 1 0-20 10 9 0 0 1 10 9 5 5 0 0 1-5 5h-2.25a1.75 1.75 0 0 0-1.4 2.8l.3.4a1.75 1.75 0 0 1-1.4 2.8z"/><circle cx="13.5" cy="6.5" r=".5" fill="currentColor"/><circle cx="17.5" cy="10.5" r=".5" fill="currentColor"/><circle cx="6.5" cy="12.5" r=".5" fill="currentColor"/><circle cx="8.5" cy="7.5" r=".5" fill="currentColor"/></svg>
   html = html.replace("#THEMES", "");
 
-  html = await minify.html(html);
+  html = await minify.html(
+    html.replace(
+      "#FOOTER",
+      `
+    <footer>
+        <nav class="container">
+            <ul>
+                <li>slidesk<span>.link</span></li>
+            </ul>
+            <ul>
+                <li>
+                    <a
+                        href="https://slidesk.github.io/slidesk-doc/"
+                        target="_blank"
+                        rel="noopener"
+                    >
+                        Documentation
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://github.com/slidesk/slidesk.link"
+                        target="_blank"
+                        rel="noopener"
+                    >
+                        Source code
+                    </a>
+                </li>
+                <li><a href="/mentions">Legal</a></li>
+            </ul>
+        </nav>
+    </footer>
+  `,
+    ),
+  );
   const glob = new Glob("*.css");
   let sha = "";
   for await (const file of glob.scanSync(`${process.cwd()}/public`)) {
