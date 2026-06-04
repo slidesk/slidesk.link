ccb3cc923873d4f5da41428c0fa0c22280df434c
Sylvain Gougouzian <sylvaingougouzian@MacBook-Pro-de-gouz.local>
2026-06-05 00:41:18 +0200

    feat: display current version in footer

--- src/html/components/footer.ts
+++ src/html/components/footer.ts
@@ -1,8 +1,11 @@
+import { version } from "../../../package.json";
+
 export const footer = `
 <footer>
   <nav class="container">
     <ul>
       <li>slidesk<span>.link</span></li>
+      <li><small>v${version}</small></li>
     </ul>
     <ul>
       <li><a href="https://github.com/slidesk/slidesk.link" target="_blank" rel="noopener">Source code</a></li>
