08c067b8352de9119511a4fa844ac9c5ed3a35c5
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2026-09-05 16:58:22 +0200

    feat: sort hosted presentations by createdAt desc

--- src/database/hostedPresentation/getByUser.ts
+++ src/database/hostedPresentation/getByUser.ts
@@ -3,4 +3,5 @@ import { db } from "../../db";
 export default async (userId: number) =>
   await db.hostedPresentation.findMany({
     where: { userId: { equals: userId } },
+    orderBy: { createdAt: "desc" },
   });
